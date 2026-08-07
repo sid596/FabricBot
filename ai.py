a5a0429fb7000cbd3a312ac245be5844288fce4a
Siddhant Fursule <siddhant0939@gmail.com>
2026-08-07 16:17:06 +0530

    Changed model from lite to flash

--- ai.py
+++ ai.py
@@ -263,7 +263,7 @@ User:
 {message}"""
 
     response = client.models.generate_content(
-    model="gemini-2.5-flash-lite",
+    model="gemini-2.5-flash",
     contents=prompt,
     config={
         "response_mime_type": "application/json",
